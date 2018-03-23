3dc7f57f9e325c6d92f7b8403079ef259740533f
lance <lance_vandy@163.com>
2018-03-23 15:14:45 +0800

    配置index页面继承base.html；通过slice筛选器，完成判断url的状态——判断是否是在访问某一页面：`request.path.slice:'7' == '/course'`，判断首页：`if request.path == '/'`

--- apps/organizations/urls.py
+++ apps/organizations/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     re_path('course/(?P<org_id>\d+.*)/', OrgCourseView.as_view(),
             name='org_course'),
     re_path('desc/(?P<org_id>\d+.*)/', OrgDescView.as_view(), name='org_desc'),
-    re_path('teacher/(?P<org_id>\d+.*)/', OrgTeacherView.as_view(),
+    re_path('org_teacher/(?P<org_id>\d+.*)/', OrgTeacherView.as_view(),
             name='org_teacher'),
 
     path('teacher/list/', TeacherListView.as_view(), name='teacher_list'),
